3626cae55e8a7570c3b13497b050d61736c7f63c
gurukumara <gurukumara@gmail.com>
2016-09-03 00:43:16 +0530

    Initial Commit

---
+++ main/__init__.py
@@ -0,0 +1,15 @@
+import time
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    context = {'ctime': time.strftime("%Y-%m-%d %H:%M:%S")}
+    return render_template("home.html", **context)
+
+
+if __name__ == '__main__':
+    app.run()
